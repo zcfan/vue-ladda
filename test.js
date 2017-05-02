9a932395d40e26ac1bccafe2134d3dc87392b171
Alan Zhang <gagazcfan@icloud.com>
2017-05-02 18:49:17 +0800

    add required prop test & default value

--- test.js
+++ test.js
@@ -1,5 +1,18 @@
+import Vue from 'vue';
+import VueLadda from './src/vue-ladda.vue';
+
 describe('VueLadda', () => {
-  it('1+1 should be 2', () => {
-    expect(1+1).toBe(2);
+  const Ctor = Vue.extend(VueLadda);
+
+  it('require given "loading" prop', () => {
+    spyOn(console, 'error').andCallFake((msg) => {
+      expect(msg.startsWith('[Vue warn]: Missing required prop: "loading"')).toBeTruthy();
+    })
+    const vm = new Ctor().$mount();
+  })
+
+  it('sets the correct default data', () => {
+    const vm = new Ctor().$mount();
+    expect(vm.dataStyle).toBe('expand-left');
   });
 });
